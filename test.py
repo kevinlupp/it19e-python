c431b0b22f49c5e370c4d81a66472026d485a90e
Kevin Lupp <kevin.lupp@tptlive.ee>
2022-03-22 09:50:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 t = [".",".",".",".",".",".",".",".","."]
 
 def dis():
+	print("  0    1    2")
 	print(t[:3])
+	print("  3    4    5")
 	print(t[3:6])
+	print("  6    7    8")
 	print(t[6:9])
 
 while True:
 	dis()
-	o = input()
-	t[o] = "o"
+	o = int(input())
+	if t[o] == ".":
+		t[o] = "o"
